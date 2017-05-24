54c6541fcd8609d6eaa184683609deb15d7a2702
Michael Warneke <michael.warneke@outlook.com.au>
2017-05-24 15:27:47 +0800

    Shared Module with Toolbar, PageNotFound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { SharedModule } from "app/modules/shared/shared.module";
+
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+
+//Material modules necessary for root (Toolbar)
+import { MdButtonModule, MdToolbarModule, MdIconModule, MdMenuModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    MdButtonModule, MdToolbarModule, MdIconModule, MdMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { PageNotFoundComponent } from "app/components/page-not-found/page-not-found.component";
+
+@NgModule({
+  imports: [
+  ],
+  exports: [
+    CommonModule,
+    PageNotFoundComponent,
+
+    ReactiveFormsModule,
+    FlexLayoutModule,
+  ],
+  declarations: [
+    PageNotFoundComponent
+  ]
+})
+export class SharedModule { }
